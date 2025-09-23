3499fef67ee547c228bc754f4acae81f6e472e37
r1zuke <bogdan.mochulskyi@gmail.com>
2025-09-23 15:00:13 +0300

    Add files via upload

---
+++ 2209/2209/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PersonalFinance
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
